3a79d08c79941a04b50ba86ef7a04b826597105f
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-11-10 14:15:18 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const app = express();
 const cors = require('cors');
 const services = require('./services.json');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_username}:${process.env.DB_password}@cluster0.1uor19o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("adornment").collection("services");
+  // perform actions on the collection object
+  console.log('inside collections')
+});
+
 
 app.get('/', (req, res)=>{
     res.send('Adornment Server is Running...')
